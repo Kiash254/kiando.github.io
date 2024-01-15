1acce0c86ed4af6204fe1e6ffc27fcd29b8b43f5
Kiash254 <codewithkiash@gmail.com>
2024-01-15 11:43:14 +0300

    renamed the index file

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -1,73 +1,37 @@
 <?php
-
-$errorMSG = "";
-
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
-}
-
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
-} else {
     $email = $_POST["email"];
-}
-
-// PHONE
-if (isset($_POST["phone_number"])) {
     $phone_number = $_POST["phone_number"];
-}
-
-// SUBJECT
-if (isset($_POST["subject"])) {
     $subject = $_POST["subject"];
-}
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
     $message = $_POST["message"];
-}
 
+    $EmailTo = "codewithkiash@gmail.com";
+    $bodySubject = "New Message Received";
 
-$EmailTo = "yourname@domain.com";
+    $Body = "";
+    $Body .= "Name: ";
+    $Body .= $name;
+    $Body .= "\n";
+    $Body .= "Email: ";
+    $Body .= $email;
+    $Body .= "\n";
+    $Body .= "Phone Number: ";
+    $Body .= $phone_number;
+    $Body .= "\n";
+    $Body .= "subject: ";
+    $Body .= $subject;
+    $Body .= "\n";
+    $Body .= "Message: ";
+    $Body .= $message;
+    $Body .= "\n";
 
-$bodySubject = "New Message Received";
+    $success = mail($EmailTo, $bodySubject, $Body);
 
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Phone Number: ";
-$Body .= $phone_number;
-$Body .= "\n";
-$Body .= "subject: ";
-$Body .= $subject;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $bodySubject, $Body);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
+    if ($success){
+       echo "success";
+    }else{
         echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
     }
 }
-
 ?>
